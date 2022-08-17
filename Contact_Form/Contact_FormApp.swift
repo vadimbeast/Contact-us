d9eccea81344ef5299e1094fd0d81db1672b04a8
vadimbeast <vadim.reklama58@yandex.ru>
2022-08-17 10:44:12 +0300

    release ver1

---
+++ Contact_Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Вадим Селиверстов on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
